38ca11584845e9d2245033f2bd0afe53062c1036
jiehfeng <alainjiehfeng@gmail.com>
2024-11-23 17:39:08 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# cached_chewie
+
+## This if a fork of official [chewie](https://pub.dev/packages/chewie) bundled with [cached_video_player](https://pub.dev/packages/cached_video_player)
+
+### Follow the documentation of official [chewie](https://pub.dev/packages/chewie) packages, except the following part reagrding your `pubspec.yaml`.
+
+#### Instead of:
+```yaml
+dependencies:
+  chewie: <latest_version>
+  video_player: <latest_version>
+```
+
+#### Use:
+```yaml
+dependencies:
+  cached_chewie: <latest_version>
+  cached_video_player: <latest_version>
+```
---
+++ lib/src/material/material_progress_bar.dart
@@ -0,0 +1,38 @@
+import 'package:cached_chewie/src/chewie_progress_colors.dart';
+import 'package:cached_chewie/src/progress_bar.dart';
+import 'package:cached_video_player/cached_video_player.dart';
+import 'package:flutter/material.dart';
+
+class MaterialVideoProgressBar extends StatelessWidget {
+  MaterialVideoProgressBar(
+    this.controller, {
+    this.height = kToolbarHeight,
+    ChewieProgressColors? colors,
+    this.onDragEnd,
+    this.onDragStart,
+    this.onDragUpdate,
+    Key? key,
+  })  : colors = colors ?? ChewieProgressColors(),
+        super(key: key);
+
+  final double height;
+  final CachedVideoPlayerController controller;
+  final ChewieProgressColors colors;
+  final Function()? onDragStart;
+  final Function()? onDragEnd;
+  final Function()? onDragUpdate;
+
+  @override
+  Widget build(BuildContext context) {
+    return VideoProgressBar(
+      controller,
+      barHeight: 10,
+      handleHeight: 6,
+      drawShadow: true,
+      colors: colors,
+      onDragEnd: onDragEnd,
+      onDragStart: onDragStart,
+      onDragUpdate: onDragUpdate,
+    );
+  }
+}
